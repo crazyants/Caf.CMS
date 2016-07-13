4999a9a3a792e20cbbf6eb682d9a6b491300ef27
Tomny <crazyants@users.noreply.github.com>
2016-07-13 16:05:54 +0800

    修改菜单模型

--- CAF.WebSite.Mvc/Administrator/Models/Common/MenuModel.cs
+++ CAF.WebSite.Mvc/Administrator/Models/Common/MenuModel.cs
@@ -5,34 +5,16 @@ using System.Web;
 
 namespace CAF.WebSite.Mvc.Admin.Models.Common
 {
-
-    public class MenuRoot
-    {
-        public MenuRoot()
-        {
-            menu = new List<MenuModel>();
-        }
-        public string id { get; set; }
-        public List<MenuModel> menu { get; set; }
-
-
-    }
-
     public class MenuModel
     {
         public MenuModel()
         {
-            items = new List<MenuItemModel>();
+            Childitems = new List<MenuModel>();
         }
-        public string text { get; set; }
-        public List<MenuItemModel> items { get; set; }
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Href { get; set; }
+        public List<MenuModel> Childitems { get; set; }
     }
-
-    public class MenuItemModel
-    {
-        public string id { get; set; }
-        public string text { get; set; }
-        public string href { get; set; }
-
-    }
+ 
 }
